7e08b522749c512f5814ab75574228b54f80972d
jhr <jhr@aformatik.de>
2022-03-30 16:31:57 +0200

    .

--- src/Main.hs
+++ src/Main.hs
@@ -2,29 +2,67 @@
 
 module Main where
 
-import           Data.Text                 (pack, replace, stripSuffix, unpack)
+import           Data.Text                 (stripPrefix, pack)
 import           Lucid
 import           Network.HTTP.Types        (status200, status404)
+import           Network.URI.Encode        (decodeText)
 import           Network.HTTP.Types.Header (hContentType)
 import           Network.Wai               (Application, Response, rawPathInfo,
-                                            responseFile, responseLBS, strictRequestBody)
+                                            responseFile, responseLBS,
+                                            strictRequestBody)
 import           Network.Wai.Handler.Warp  (run)
 import           Prelude                   hiding (ByteString, length)
 import           Types
+import Data.Char (isNumber)
+import           Text.Megaparsec      hiding (chunk)
+import           Text.Megaparsec.Char (char, space, string)
+
 
 main :: IO ()
 main = run 3003 serve
   where
     serve request respond = do
+        print $ rawPathInfo request
+        print =<< strictRequestBody request
         response <- case rawPathInfo request of
-                "/post" -> page . Just . parseNumber <$> strictRequestBody request 
-                _       -> return $ page Nothing
+            "/post" -> page . Just . parseNumber <$> strictRequestBody request
+            _       -> return $ page Nothing
         respond $ responseLBS status200 [(hContentType, "text/html")] $ renderBS response
 
 parseNumber :: LByteString -> Html ()
-parseNumber input = div_ $ do 
-   h1_ "Ergebnis:"
-   p_ "TODO"
+parseNumber input = div_ $ do
+    h1_ "Ergebnis:"
+    let input = decodeText . toText $ input
+    --     input' = stripPrefix "number=" input ?: input
+    -- case parse pNumber "Eingabe" input of
+    --     Right s -> code_ . toHtml $ (show s :: String)
+    --     Left e -> code_ . toHtml $ errorBundlePretty e
+    code_ $ toHtml input
+
+data LandISOCode = DE deriving (Show)
+
+data Number = Number
+    { landVorwahl  :: LandISOCode
+    , stadtVorwahl :: Text
+    , num          :: Text
+    , durchwahl    :: Maybe Text
+    }
+    deriving (Show)
+
+type Parser = Parsec Void Text
+
+pNumber :: Parser Number
+pNumber = do
+    vorwahl <- optional $ DE <$ "+49"
+    -- let a = (if isJust vorwahl then option [] (pure <$> char '0') else pure <$> char '0')
+    -- city <- (++) <$> a <*> count 4 (satisfy isNumber)
+    -- optional sep
+    -- num <- count 6 (satisfy isNumber)
+    -- durchwahl <- optional $ optional sep *> count 3 (satisfy isNumber)
+    return $ Number (vorwahl ?: DE) "" "" (Just "") -- (pack city) (pack num) (pack <$> durchwahl)
+
+sep :: Parser ()
+sep = void $ oneOf ['/', '-']
 
 page :: Maybe (Html ()) -> Html ()
 page solution = html_ $ do
