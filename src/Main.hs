9cc4d2ed224d6136f5ad5ba5b19cacc61d9cbc3e
jhr <jhr@aformatik.de>
2022-03-30 14:47:35 +0200

    Haskell Prototyp

--- src/Main.hs
+++ src/Main.hs
@@ -1,43 +1,41 @@
 {-# LANGUAGE NoImplicitPrelude #-}
-{-# LANGUAGE RecordWildCards   #-}
 
 module Main where
 
-import           Data.Text                       (pack, replace, stripSuffix,
-                                                  unpack)
-import           Lucid                           (Html, renderBS)
-import           Network.HTTP.Types              (status200, status404)
-import           Network.HTTP.Types.Header       (hContentType)
-import           Network.Wai                     (Application, Response,
-                                                  rawPathInfo, responseFile,
-                                                  responseLBS)
-import           Network.Wai.Handler.Warp        (run)
-import           Prelude                         hiding (ByteString, length)
+import           Data.Text                 (pack, replace, stripSuffix, unpack)
+import           Lucid
+import           Network.HTTP.Types        (status200, status404)
+import           Network.HTTP.Types.Header (hContentType)
+import           Network.Wai               (Application, Response, rawPathInfo,
+                                            responseFile, responseLBS, strictRequestBody)
+import           Network.Wai.Handler.Warp  (run)
+import           Prelude                   hiding (ByteString, length)
 import           Types
 
 main :: IO ()
-main = run 3003 serve where
-    serve request respond = respond =<< selectRoute
+main = run 3003 serve
+  where
+    serve request respond = do
+        response <- case rawPathInfo request of
+                "/post" -> page . Just . parseNumber <$> strictRequestBody request 
+                _       -> return $ page Nothing
+        respond $ responseLBS status200 [(hContentType, "text/html")] $ renderBS response
 
-    sanitizedPath = B.stripPrefix "/" (rawPathInfo request) ?: B.empty
-    segments = B.split '/' sanitizedPath
-    htmlOk = responseLBS status200 [(hContentType, "text/html")] . renderBS
-    html404 = responseLBS status404 [(hContentType, "text/html")] (renderBS err404)
-    findSong :: B.ByteString -> Maybe B.ByteString -> Response
-    findSong name key = htmlOk $
-        fromMaybe err404 $ do
-            song' <- find ((== B.unpack name) . chordProFile) songs
-            key' <- maybe (Just Nothing) (Just . parseKeyMaybe . replace "%23" "#" . decodeUtf8) key
-            return $ song env song' key'
-    selectRoute = do
-        print segments
-        return $ case segments of
-            [] -> htmlOk $ mainpage env
-            ["songs", name] -> findSong name Nothing
-            ["songs", name, key] -> findSong name (if B.null key then Nothing else Just key)
-            ["favicon.ico"] ->
-                case favicon of
-                    Just path -> responseFile status200 [(hContentType, "image/x-icon")] path Nothing
-                    Nothing -> html404
-            _ -> html404
+parseNumber :: LByteString -> Html ()
+parseNumber input = div_ $ do 
+   h1_ "Ergebnis:"
+   p_ "TODO"
 
+page :: Maybe (Html ()) -> Html ()
+page solution = html_ $ do
+    head_ $ do
+        meta_ [charset_ "UTF-8"]
+        meta_ [name_ "viewport", content_ "width=device-width, initial-scale=1.0"]
+        title_ "Telefonnummer"
+    body_ $ do
+        fromMaybe mempty solution
+        br_ []
+        h1_ "Telefonnummer eingeben:"
+        form_ [method_ "POST", action_ "/post"] $ do
+            input_ [name_ "number", type_ "tel"]
+            input_ [type_ "submit"]
